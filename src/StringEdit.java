21e90364e001a3dc38e75bfac50b5c4b63a2e57f
LHS <LHS@DESKTOP-A48G9NV>
2017-07-17 11:39:19 +0900

    메인에 있던 부분들을 1차적으로 메소드로 바꿈

--- src/StringEdit.java
+++ src/StringEdit.java
@@ -5,25 +5,37 @@ public class StringEdit {
 
 		inputText = "Do you know Doctor who?";
 		String[] result = inputText.split(" ");
-		int[] alphabetArr = new int[26];
 
 		System.out.println("입력 문자열: " + inputText);
+		reverse(result);
+		checkAlphabet(inputText);
+		
+	}
+	//TODO: 메소드들을 좀더 분리 할것 & 변수 이름 손볼것
+	public static void reverse(String[] result){
+		///////////////////////
+		//입력 받은 문자열을 역순으로 출력 & 마지막 단어에 있는 문장의 끝을 의마하는 '?','!',';','.' 특수 문자만 역순에서도 마지막 단어에 따라오게 만든다.
 		int last1 = result.length - 1;
 		int last = result[last1].length() - 1;
-
 		if ((result[last1].charAt(last) == '?') || (result[last1].charAt(last) == '!')
 				|| (result[last1].charAt(last) == ';') || (result[last1].charAt(last) == '.')) {
-			result[0] = result[0] + result[last1].charAt(last);
-			result[last1] = result[last1].substring(0, last);
+			result[0] = result[0] + result[last1].charAt(last);						//역순으로 가장 뒤에 올 단어의 마지막에 특수문자 삽입
+			result[last1] = result[last1].substring(0, last);						//기존에 가장 마지막에 있던 특수 문자 삭제
 		}
 		System.out.print("출력 문자열: ");
 
 		for (int i = result.length - 1; i >= 0; i--) {
 			System.out.printf("%s ", result[i]);
 		}
-		System.out.println();
-		
+		////////////////////////////출력
+		System.out.println();	
+	}
+	
+	public static void checkAlphabet(String inputText){
+		////////알파벳 수 세기
+		int[] alphabetArr = new int[26];
 		int count = 0;
+		//총 알파벳 수 출력
 		for (int i = 0; i < inputText.length(); i++) {
 			char alphabet = inputText.charAt(i);
 			if (('a' <= alphabet && alphabet <= 'z') || ('A' <= alphabet && alphabet <= 'Z')) {
@@ -37,10 +49,13 @@ public class StringEdit {
 		}
 		System.out.println("전체 수: " + count);
 
+		//알파벳 빈도 체크
+		//TODO: 빈도가 높을 수록 먼저 나오게 만들기
 		for (int i = 0; i < alphabetArr.length; i++) {
 			if (alphabetArr[i] > 0) {
 				System.out.println((char) (i + 65) + ": " + alphabetArr[i]);
 			}
 		}
+		//////////////////////////
 	}
 }
